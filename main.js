71b9b6beff90a2e00f786a2c9eaae57ec63df959
Kelly Stoops <kstoops815@gmail.com>
2017-08-22 21:20:09 -0500

    finished exercise

--- main.js
+++ main.js
@@ -27,14 +27,16 @@ var planets = [{
 
 var planetHolderDiv = document.getElementById('planetHolder');
 var getPlanetsButton = document.getElementById('showButton');
+var inputField = document.getElementById('searchText');
+var clearButton = document.getElementById('clearButton');
 
-function domString() {
+function domString(planetz) {
   var planetString = '';
-  for(var i=0; i<planets.length; i++){
+  for(var i=0; i<planetz.length; i++){
     var newPlanet="";
     newPlanet+=`<div class="planetBox" id="planetBox-${i}">`;   
-    newPlanet+=`<div class="planetName">${planets[i].name}</div>`;
-    newPlanet+=`<img class="planetImage" src="${planets[i].url}">`;
+    newPlanet+=`<div class="planetName hidden">${planetz[i].name}</div>`;
+    newPlanet+=`<img class="planetImage" src="${planetz[i].url}">`;
     newPlanet+=`</div>`;
     planetString += newPlanet;
   }
@@ -47,9 +49,40 @@ function writeToDom(strang) {
 }
 
 getPlanetsButton.addEventListener('mouseenter', function(){
-  domString();
+  domString(planets);
 });
 
+function showMe(e){
+  //console.log("showMe event", e.target.previousSibling.classList.remove('hidden'));
+  e.target.previousSibling.classList.remove('hidden')
+}
 
 
+document.body.addEventListener('click', function(event){
+  //console.log("click event");
+  //console.log("event", event.target.parentNode.parentNode.parentNode);
+  if(event.target.className === "planetImage"){
+    console.log("yaaa");
+    showMe(event);
+  }
+})
+
+inputField.addEventListener('keypress', function(event){
+  //console.log("event", event.key);
+  if(event.key === 'Enter'){
+    var txt = inputField.value;
+    //1.filter planets array
+    var results = planets.filter(function(thing){
+      console.log("filter thing", thing);
+      return thing.name.indexOf(txt)>-1;
+    })
+    //2. domString
+      domString(results);
+  }
+})
+
+clearButton.addEventListener('click', function(){
+  inputField.value="";
+  //writeToDom('');
+});
 
